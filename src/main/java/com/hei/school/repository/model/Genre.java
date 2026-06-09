6f43af17070b1a663808d3cd4453d845abc1ff98
chiliTsila <hei.chili.4@gmail.com>
2026-06-09 20:27:45 +0300

    Create Genre class

---
+++ src/main/java/com/hei/school/repository/model/Genre.java
@@ -0,0 +1,16 @@
+package com.hei.school.repository.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Genre {
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private List<Book> books;
+}
