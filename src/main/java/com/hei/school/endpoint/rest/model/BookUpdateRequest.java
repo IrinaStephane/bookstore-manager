c1ea92d0ff6d6dc02332a903544fc837e8e3d0df
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2026-06-10 09:24:21 +0300

    feat : add BookUpdateRequest dto and Book controller

---
+++ src/main/java/com/hei/school/endpoint/rest/model/BookUpdateRequest.java
@@ -0,0 +1,17 @@
+package com.hei.school.endpoint.rest.model;
+
+import com.hei.school.entity.enums.Language;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class BookUpdateRequest {
+    @NotBlank private String title;
+    private String description;
+    @NotNull private Language language;
+    @NotEmpty private List<Long> authorIds;
+    private List<Long> genreIds;
+}
