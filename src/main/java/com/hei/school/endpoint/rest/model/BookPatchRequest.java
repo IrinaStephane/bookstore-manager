b494c613dcfda7278d27541ffbe1b992b305d06f
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2026-06-10 09:25:18 +0300

    feat : add BookPatchRequest model

---
+++ src/main/java/com/hei/school/endpoint/rest/model/BookPatchRequest.java
@@ -0,0 +1,14 @@
+package com.hei.school.endpoint.rest.model;
+
+import com.hei.school.entity.enums.Language;
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class BookPatchRequest {
+    private String title;
+    private String description;
+    private Language language;
+    private List<Long> authorIds;
+    private List<Long> genreIds;
+}
