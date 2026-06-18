ba129a37a121d0e2d91fbf88ae1fc371a2d2bfa0
Mena845 <tsikiniainatsilavina@gmail.com>
2026-06-18 11:10:41 +0300

    feat(repository): add pagination support for author search by name

--- src/main/java/com/hei/school/repository/AuthorRepository.java
+++ src/main/java/com/hei/school/repository/AuthorRepository.java
@@ -2,6 +2,11 @@ package com.hei.school.repository;
 
 import com.hei.school.entity.Author;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuthorRepository extends JpaRepository<Author, UUID> {}
+public interface AuthorRepository extends JpaRepository<Author, UUID> {
+  Page<Author> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(
+          String firstName, String lastName, Pageable pageable);
+}
