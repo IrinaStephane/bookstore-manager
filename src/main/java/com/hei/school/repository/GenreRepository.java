044b80ca0ea8c11de24df2c2084a4dead3903beb
Mena845 <tsikiniainatsilavina@gmail.com>
2026-06-18 11:11:32 +0300

    feat(repository): add method to check if genre exists by name (case insensitive)

--- src/main/java/com/hei/school/repository/GenreRepository.java
+++ src/main/java/com/hei/school/repository/GenreRepository.java
@@ -4,4 +4,6 @@ import com.hei.school.entity.Genre;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GenreRepository extends JpaRepository<Genre, UUID> {}
+public interface GenreRepository extends JpaRepository<Genre, UUID> {
+    boolean existsByNameIgnoreCase(String name);
+}
