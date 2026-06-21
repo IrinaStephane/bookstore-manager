6241dd04d53da3aa277a8b6ecbaadb8f5f02879e
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2026-06-21 23:25:37 +0300

    refactor : format code

--- src/test/java/com/hei/school/conf/PostgresConf.java
+++ src/test/java/com/hei/school/conf/PostgresConf.java
@@ -3,6 +3,7 @@ package com.hei.school.conf;
 import com.hei.school.PojaGenerated;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.postgresql.PostgreSQLContainer;
+
 @PojaGenerated
 public class PostgresConf {
 
