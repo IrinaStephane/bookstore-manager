a9f961003e2862e13eca14a8b8abc07ee50bd3c1
Mandanirina Tiavina <solontianamandanirina@gmail.com>
2026-07-09 09:20:06 +0300

    Feat/s3 (#22)
    * feat: implement s3 bucketComponent
    
    * fix: format
    
    * fix: put Ar in the end of total amount
    
    * fix: add edition name for insufficiency exception error
    
    * fix: format

--- src/main/java/com/hei/school/service/event/SaleConfirmedEventService.java
+++ src/main/java/com/hei/school/service/event/SaleConfirmedEventService.java
@@ -68,6 +68,7 @@ public class SaleConfirmedEventService implements Consumer<SaleConfirmedEvent> {
             + "</tbody></table>"
             + "<h3 style=\"text-align:right;\">Total: "
             + String.format("%.2f", sale.getTotalAmount())
+            + " Ar"
             + "</h3>";
 
     File receiptPdf = pdfGenerator.generate(buildReceiptHtml(sale, tableHtml), "sale-receipt-");
