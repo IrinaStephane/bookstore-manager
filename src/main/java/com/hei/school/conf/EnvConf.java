353fde1ad8a403739276d75c2dea86db705f230b
Mena845 <tsikiniainatsilavina@gmail.com>
2026-07-09 22:27:37 +0300

    feat: add EnvConf class to configure test API key for dynamic properties

---
+++ src/main/java/com/hei/school/conf/EnvConf.java
@@ -0,0 +1,12 @@
+package com.hei.school.conf;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+
+public class EnvConf {
+
+    public static final String TEST_API_KEY = "test-api-key";
+
+    public void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("app.api-key", () -> TEST_API_KEY);
+    }
+}
