41ba6e6676903f10f14da5fbd2cd215c597e6e56
Manda Tiavina <solontianamandanirina@gmail.com>
2026-07-09 09:09:59 +0300

    fix: put Ar in the end of total amount

--- src/main/java/com/hei/school/service/event/SaleConfirmedEventService.java
+++ src/main/java/com/hei/school/service/event/SaleConfirmedEventService.java
@@ -67,7 +67,7 @@ public class SaleConfirmedEventService implements Consumer<SaleConfirmedEvent> {
             + itemsHtml
             + "</tbody></table>"
             + "<h3 style=\"text-align:right;\">Total: "
-            + String.format("%.2f", sale.getTotalAmount())
+            + String.format("%.2f", sale.getTotalAmount()) + " Ar"
             + "</h3>";
 
     File receiptPdf = pdfGenerator.generate(buildReceiptHtml(sale, tableHtml), "sale-receipt-");
