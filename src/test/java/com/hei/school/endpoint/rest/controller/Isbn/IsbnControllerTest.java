f3b2c1dc46ec3329a1bc0c355e04de4fa7ccc970
Irina <irinarakoto2005@gmail.com>
2026-07-15 20:52:01 +0300

    test: add unit tests for ISBN lookup endpoint

---
+++ src/test/java/com/hei/school/endpoint/rest/controller/Isbn/IsbnControllerTest.java
@@ -0,0 +1,56 @@
+package com.hei.school.endpoint.rest.controller.Isbn;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.hei.school.exception.ResourceNotFoundException;
+import com.hei.school.service.IsbnLookupService;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(IsbnController.class)
+class IsbnControllerTest {
+
+  @Autowired private MockMvc mockMvc;
+  @MockBean private IsbnLookupService isbnLookupService;
+
+  @Test
+  void lookupIsbnShouldReturnBookData() throws Exception {
+    Map<String, Object> authors =
+        Map.of("url", "https://openlibrary.org/authors/OL1A", "name", "Test Author");
+    Map<String, Object> bookData = new LinkedHashMap<>();
+    bookData.put("url", "https://openlibrary.org/books/OL1M");
+    bookData.put("key", "/books/OL1M");
+    bookData.put("title", "Test Book");
+    bookData.put("authors", List.of(authors));
+    bookData.put("number_of_pages", 300);
+
+    given(isbnLookupService.lookup("9781234567890"))
+        .willReturn(Map.of("ISBN:9781234567890", bookData));
+
+    mockMvc
+        .perform(get("/isbn/9781234567890"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$['ISBN:9781234567890'].title").value("Test Book"))
+        .andExpect(jsonPath("$['ISBN:9781234567890'].key").value("/books/OL1M"))
+        .andExpect(jsonPath("$['ISBN:9781234567890'].url").value("https://openlibrary.org/books/OL1M"))
+        .andExpect(jsonPath("$['ISBN:9781234567890'].authors[0].name").value("Test Author"))
+        .andExpect(jsonPath("$['ISBN:9781234567890'].number_of_pages").value(300));
+  }
+
+  @Test
+  void lookupIsbnShouldReturn404WhenNotFound() throws Exception {
+    given(isbnLookupService.lookup("0000000000"))
+        .willThrow(new ResourceNotFoundException("No book found for ISBN: 0000000000"));
+
+    mockMvc.perform(get("/isbn/0000000000")).andExpect(status().isNotFound());
+  }
+}
---
+++ src/test/java/com/hei/school/service/IsbnLookupServiceImplTest.java
@@ -0,0 +1,162 @@
+package com.hei.school.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+
+import com.hei.school.exception.ResourceNotFoundException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+@ExtendWith(MockitoExtension.class)
+class IsbnLookupServiceImplTest {
+
+  @Mock private RestTemplate restTemplate;
+  @InjectMocks private IsbnLookupServiceImpl isbnLookupService;
+
+  @BeforeEach
+  void setUp() {
+    ReflectionTestUtils.setField(isbnLookupService, "googleBooksApiKey", "");
+  }
+
+  @Test
+  void lookupShouldReturnOpenLibraryData() {
+    String isbn = "9781234567890";
+    Map<String, Object> openLibraryResponse = new LinkedHashMap<>();
+    openLibraryResponse.put("key", "/books/OL1M");
+    openLibraryResponse.put("title", "Test Book");
+    openLibraryResponse.put("number_of_pages", 300);
+    openLibraryResponse.put("authors", List.of(Map.of("key", "/authors/OL1A")));
+
+    Map<String, Object> authorResponse = Map.of("name", "Test Author");
+
+    given(restTemplate.getForObject("https://openlibrary.org/isbn/" + isbn + ".json", Map.class))
+        .willReturn(openLibraryResponse);
+    given(
+            restTemplate.getForObject(
+                "https://openlibrary.org/authors/OL1A.json", Map.class))
+        .willReturn(authorResponse);
+
+    Map<String, Object> result = isbnLookupService.lookup(isbn);
+
+    assertTrue(result.containsKey("ISBN:" + isbn));
+    @SuppressWarnings("unchecked")
+    Map<String, Object> data = (Map<String, Object>) result.get("ISBN:" + isbn);
+    assertEquals("Test Book", data.get("title"));
+    assertEquals("/books/OL1M", data.get("key"));
+    assertEquals("https://openlibrary.org/books/OL1M", data.get("url"));
+    assertEquals(300, data.get("number_of_pages"));
+
+    @SuppressWarnings("unchecked")
+    List<Map<String, Object>> authors = (List<Map<String, Object>>) data.get("authors");
+    assertNotNull(authors);
+    assertEquals(1, authors.size());
+    assertEquals("Test Author", authors.get(0).get("name"));
+    assertEquals("https://openlibrary.org/authors/OL1A", authors.get(0).get("url"));
+  }
+
+  @Test
+  void lookupShouldStripNonDigitCharacters() {
+    String isbn = "978-1-234-56789-0";
+    String cleanIsbn = "9781234567890";
+
+    Map<String, Object> openLibraryResponse = new LinkedHashMap<>();
+    openLibraryResponse.put("key", "/books/OL1M");
+    openLibraryResponse.put("title", "Test Book");
+    openLibraryResponse.put("number_of_pages", 200);
+
+    given(restTemplate.getForObject("https://openlibrary.org/isbn/" + cleanIsbn + ".json", Map.class))
+        .willReturn(openLibraryResponse);
+
+    Map<String, Object> result = isbnLookupService.lookup(isbn);
+
+    assertTrue(result.containsKey("ISBN:" + cleanIsbn));
+  }
+
+  @Test
+  void lookupShouldFallbackToGoogleBooksWhenOpenLibraryNotFound() {
+    String isbn = "9780000000000";
+
+    given(restTemplate.getForObject("https://openlibrary.org/isbn/" + isbn + ".json", Map.class))
+        .willThrow(new HttpClientErrorException(org.springframework.http.HttpStatus.NOT_FOUND));
+
+    Map<String, Object> volumeInfo = new LinkedHashMap<>();
+    volumeInfo.put("title", "Google Book");
+    volumeInfo.put("pageCount", 150);
+    volumeInfo.put("authors", List.of("G Author"));
+
+    Map<String, Object> googleItem = Map.of("volumeInfo", volumeInfo);
+    Map<String, Object> googleResponse = Map.of("items", List.of(googleItem));
+
+    given(
+            restTemplate.getForObject(
+                eq("https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn), eq(Map.class)))
+        .willReturn(googleResponse);
+
+    Map<String, Object> result = isbnLookupService.lookup(isbn);
+
+    assertTrue(result.containsKey("ISBN:" + isbn));
+    @SuppressWarnings("unchecked")
+    Map<String, Object> data = (Map<String, Object>) result.get("ISBN:" + isbn);
+    assertEquals("Google Book", data.get("title"));
+    assertEquals(150, data.get("number_of_pages"));
+  }
+
+  @Test
+  void lookupShouldThrowWhenBothApisFail() {
+    String isbn = "9780000000000";
+
+    given(restTemplate.getForObject("https://openlibrary.org/isbn/" + isbn + ".json", Map.class))
+        .willThrow(new HttpClientErrorException(org.springframework.http.HttpStatus.NOT_FOUND));
+
+    Map<String, Object> googleResponse = Map.of();
+
+    given(
+            restTemplate.getForObject(
+                eq("https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn), eq(Map.class)))
+        .willReturn(googleResponse);
+
+    assertThrows(ResourceNotFoundException.class, () -> isbnLookupService.lookup(isbn));
+  }
+
+  @Test
+  void lookupShouldEnrichAuthorsWithUrlAndName() {
+    String isbn = "9781234567890";
+    Map<String, Object> openLibraryResponse = new LinkedHashMap<>();
+    openLibraryResponse.put("key", "/books/OL1M");
+    openLibraryResponse.put("title", "Multi Author Book");
+    openLibraryResponse.put(
+        "authors",
+        List.of(Map.of("key", "/authors/OL1A"), Map.of("key", "/authors/OL2A")));
+
+    given(restTemplate.getForObject("https://openlibrary.org/isbn/" + isbn + ".json", Map.class))
+        .willReturn(openLibraryResponse);
+    given(restTemplate.getForObject("https://openlibrary.org/authors/OL1A.json", Map.class))
+        .willReturn(Map.of("name", "Author One"));
+    given(restTemplate.getForObject("https://openlibrary.org/authors/OL2A.json", Map.class))
+        .willReturn(Map.of("name", "Author Two"));
+
+    Map<String, Object> result = isbnLookupService.lookup(isbn);
+
+    @SuppressWarnings("unchecked")
+    Map<String, Object> data = (Map<String, Object>) result.get("ISBN:" + isbn);
+    @SuppressWarnings("unchecked")
+    List<Map<String, Object>> authors = (List<Map<String, Object>>) data.get("authors");
+    assertEquals(2, authors.size());
+    assertEquals("Author One", authors.get(0).get("name"));
+    assertEquals("https://openlibrary.org/authors/OL1A", authors.get(0).get("url"));
+    assertEquals("Author Two", authors.get(1).get("name"));
+    assertEquals("https://openlibrary.org/authors/OL2A", authors.get(1).get("url"));
+  }
+}
